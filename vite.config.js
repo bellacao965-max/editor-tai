d777eb0faea890bfc36f73ca1c1692b229182122
bellacao965-max <bellacao965@gmail.com>
2025-11-23 14:39:02 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+iimport { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
-})
+  server: {
+    host: true,
+  },
+  build: {
+    outDir: "dist",
+  },
+});
